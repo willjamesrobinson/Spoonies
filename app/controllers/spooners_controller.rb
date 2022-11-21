85fea634a6670d081233eeb6141c0ad377b70a9d
Ariani Plukaard <ari.plukaard@gmail.com>
2022-11-21 16:02:48 +1100

    created new, create and show for spooner

--- app/controllers/spooners_controller.rb
+++ app/controllers/spooners_controller.rb
@@ -3,9 +3,26 @@ class SpoonersController < ApplicationController
     @spooners = Spooner.all
   end
 
+  def show
+    @spooner = Spooner.find(params[:id])
+  end
+
   def new
+    @spooner = Spooner.new
   end
 
   def create
+    @spooner = Spooner.new(spooner_params)
+    if @spooner.save
+      redirect_to spooner_path(@spooner)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def spooner_params
+    params.require(:spooner).permit(:name, :age, :spoon_type, :gender, :price, :overview, :img)
   end
 end
--- app/models/spooner.rb
+++ app/models/spooner.rb
@@ -1,7 +1,7 @@
 class Spooner < ApplicationRecord
   SPOON_TYPE = ["big spoon", "little spoon", "any spoon"]
   GENDER = ["male", "female", "other"]
-  validates :name, :spoon_type, :price, presence: true
+  validates :name, :spoon_type, :price, :gender, :age, presence: true
   validates :spoon_type, inclusion: { in: SPOON_TYPE }
   validates :gender, inclusion: { in: GENDER }
   validates :price, :age, numericality: { only_integer: true }
