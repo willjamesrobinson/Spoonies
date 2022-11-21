5c48db8a0471046131015c3b6cb0f92727a430d5
Ariani Plukaard <ari.plukaard@gmail.com>
2022-11-21 17:58:11 +1100

    added edit, update and delete to spooner, not sensitive to login/current_user yet

--- app/controllers/spooners_controller.rb
+++ app/controllers/spooners_controller.rb
@@ -1,12 +1,12 @@
 class SpoonersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-  
+  skip_before_action :authenticate_user!, only: [:home, :index, :show, :new, :create, :edit, :update, :destroy]
+  before_action :set_spooner, only: [:show, :edit, :update, :destroy]
+
   def index
     @spooners = Spooner.all
   end
 
   def show
-    @spooner = Spooner.find(params[:id])
   end
 
   def new
@@ -21,14 +21,30 @@ class SpoonersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
-  def home
 
+  def home
   end
-  
+
+  def edit
+  end
+
+  def update
+    @spooner.update(spooner_params)
+    redirect_to spooner_path(@spooner)
+  end
+
+  def destroy
+    @spooner.destroy
+    redirect_to spooners_path, status: :see_other
+  end
+
   private
 
   def spooner_params
     params.require(:spooner).permit(:name, :age, :spoon_type, :gender, :price, :overview, :img)
+  end
+
+  def set_spooner
+    @spooner = Spooner.find(params[:id])
   end
 end
