c44b2fb6049be0f48697f910de8ab8cac5a2f457
Lachlan Bennetts <lachlanbennetts@hotmail.com>
2022-11-22 10:54:08 +1100

    LogIn LogOut alternation works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: :home
   before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :authenticate_user!, only: :home
 
+  private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :gender, :spoon_type])
 
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :gender, :spoon_type])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :gender, :spoon_type])
+  end
+
+  def sing_up_params
+    params.require(:user).permit(:first_name, :age, :gender, :spoon_type, :email, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_034300) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_211431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "date"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "spooners", force: :cascade do |t|
     t.string "name"
@@ -34,8 +43,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_034300) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
+    t.string "first_name"
+    t.string "gender"
+    t.string "spoon_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
 end
