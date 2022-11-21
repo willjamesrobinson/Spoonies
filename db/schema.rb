611273ed683d9178da65d84760d9c28bc9a8de30
Anna Pitt <annapittcodes@gmail.com>
2022-11-21 16:43:12 +1100

    footer attempt 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_030806) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_034300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "spooners", force: :cascade do |t|
     t.string "name"
     t.integer "age"
