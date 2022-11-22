1e6e3548cb482a209cd0fe8118c5e390191e7f23
Ariani Plukaard <ari.plukaard@gmail.com>
2022-11-22 17:04:03 +1100

    fixed delete spooner and image replacement in the edit for spooner

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'faker'
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -10,12 +11,19 @@ require 'faker'
 
 10.times do
   details = {
-    name: Faker::Name.first_name,
+    name: Faker::Superhero.name,
     age: rand(18..50),
     spoon_type: ["big spoon", "little spoon", "any spoon"].sample,
     gender: ["male", "female"].sample,
     price: rand(2..100),
     overview: Faker::Lorem.paragraph
   }
-  Spooner.create(details)
+  spooner = Spooner.create(details)
+  file1 = URI.open("https://source.unsplash.com/random/?person")
+  spooner.img.attach(io: file1, filename: "#{spooner.name}1.jpg", content_type: "image/jpg")
+  file2 = URI.open("https://source.unsplash.com/random/?person")
+  spooner.img.attach(io: file2, filename: "#{spooner.name}2.jpg", content_type: "image/jpg")
+  file3 = URI.open("https://source.unsplash.com/random/?person")
+  spooner.img.attach(io: file3, filename: "#{spooner.name}3.jpg", content_type: "image/jpg")
+  spooner.save
 end
