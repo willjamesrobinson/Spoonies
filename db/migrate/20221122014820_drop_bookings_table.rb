3f17a53595e71a90c1fa89d2439da21cd212ce3f
will robinson <willjamesr@gmail.com>
2022-11-22 17:33:55 +1100

    doodabeeedoo

--- db/migrate/20221122014820_drop_bookings_table.rb
+++
@@ -1,5 +0,0 @@
-class DropBookingsTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table(:bookings)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,17 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_015408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bookings", force: :cascade do |t|
-    t.string "date"
-    t.string "details"
-    t.bigint "user_id", null: false
-    t.bigint "spooner_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["spooner_id"], name: "index_bookings_on_spooner_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -51,6 +40,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_015408) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "date"
+    t.string "details"
+    t.bigint "user_id", null: false
+    t.bigint "spooner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["spooner_id"], name: "index_bookings_on_spooner_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "spooners", force: :cascade do |t|
@@ -81,10 +81,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_015408) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookings", "spooners"
-  add_foreign_key "bookings", "users"
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
+  add_foreign_key "bookings", "spooners"
+  add_foreign_key "bookings", "users"
 end
