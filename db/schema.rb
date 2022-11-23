107c0b2d690d074bac441c4a0b753d3d329bb99e
Lachlan Bennetts <lachlanbennetts@hotmail.com>
2022-11-23 14:53:56 +1100

    Finally fixed the login page, then did some bookming thingies, more to do though

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_015408) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_031156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_015408) do
     t.bigint "spooner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "pending", default: true
     t.index ["spooner_id"], name: "index_bookings_on_spooner_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
