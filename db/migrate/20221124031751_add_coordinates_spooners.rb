43a529f7bae642dd80e631e90d0da6192c3b26ba
will robinson <willjamesr@gmail.com>
2022-11-25 15:43:22 +1100

    fix

--- db/migrate/20221124031751_add_coordinates_spooners.rb
+++ db/migrate/20221124031751_add_coordinates_spooners.rb
@@ -2,6 +2,5 @@ class AddCoordinatesSpooners < ActiveRecord::Migration[7.0]
   def change
     add_column :spooners, :latitude, :float
     add_column :spooners, :longitude, :float
-    add_column :spooners, :address
   end
 end
---
+++ db/migrate/20221125044214_add_address.rb
@@ -0,0 +1,5 @@
+class AddAddress < ActiveRecord::Migration[7.0]
+  def change
+    add_column :spooners, :address
+  end
+end
